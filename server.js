8626cca29f4efa4a5228edecb2f630c3dec93bc2
SoraSpades <davidandrino2002@gmail.com>
2021-09-01 00:13:40 +0200

    Add mongoose connect options

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const mongodb = process.env.mongod || 'mongodb://localhost/vgames';
 const port = process.env.port || 3000;
 
-mongoose.connect(mongodb, (err) => {
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) throw err;
 
   if (err) {
