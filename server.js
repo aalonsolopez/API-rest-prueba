08cc6b579e6c37538e6c345e096f4765bbb34a1d
k33per <49404378+AlexK33per@users.noreply.github.com>
2021-10-01 23:04:12 +0200

    corrected some useless options on server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const mongodb = process.env.mongod || 'mongodb://localhost/vgames';
 const port = process.env.port || 3000;
 
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect(mongodb, (err) => {
   if (err) throw err;
 
   if (err) {
